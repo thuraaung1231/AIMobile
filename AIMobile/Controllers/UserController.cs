3da0da7c561f3f92917eef067e80af2c558aa808
AyeNaingPhyoe <152674167+AyeNaingPhyoe@users.noreply.github.com>
2024-02-22 21:26:54 +0630

    Test

---
+++ AIMobile/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIMobile.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
