f0c6f3082a43fe128ff0071904ffbdd737d9b5e6
Nandar Aye <160568045+nandaraye66@users.noreply.github.com>
2024-02-28 12:09:58 +0630

    I remove File

--- AIMobile/DAO/ApplicationDbContext.cs
+++ AIMobile/DAO/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AIMobile.DAO
 {
-    public class ApplicationDbContext :IdentityDbContext<IdentityUser,IdentityRole,string>
+    public class ApplicationDbContext :DbContext
     {
 public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
        public DbSet<BrandEntity> Brand { get; set; }
