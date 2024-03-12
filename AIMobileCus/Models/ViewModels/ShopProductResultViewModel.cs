a5e62a56a081b1c472704749c931fe5af055053f
Wai Phyo Aung <waung0248@gmail.com>
2024-03-12 12:03:01 +0630

    I add the requirement

--- AIMobileCus/Models/ViewModels/ShopProductResultViewModel.cs
+++ AIMobileCus/Models/ViewModels/ShopProductResultViewModel.cs
@@ -2,18 +2,13 @@ namespace AIMobileCus.Models.ViewModels
 {
     public class ShopProductResultViewModel
     {
-        public string Id { get; set; }
-        public string TypeId { get; set; }
-        public string ShopId { get; set; }
-        public string ShopName { get; set; }
+        public string ShopProductId { get; set; }
+       
         public string ProductId { get; set; }
-        public string ProductName { get; set; }
         public string ImageId { get; set; }
-        public string ImageName { get; set; }
-        public string Description { get; set; }
-        public int StockCount { get; set; }
-        public decimal Unitprice { get; set;}
-        public decimal UnitPrice { get; set; }
+        public string Name { get; set; }
+        public string FrontImageUrl { get; set; }
+        
 
     }
 }
